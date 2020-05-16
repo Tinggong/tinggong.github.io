0d0f702d92e58f6e44fdb58133a207e61a72aebc
Ting Gong <tinggong@carolynas-mbp.home>
2020-05-16 14:34:49 +0100

    built page

---
+++ _pages/about.md
@@ -0,0 +1,6 @@
+---
+title: "About"
+permalink: /about/
+---
+
+This is a personal page hosted by Ting Gong, PhD candidate working on microstructure imaging with dMRI.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "CNN for fast fODF",
+        "excerpt":"Fast learning of fiber orientation distribution function for MR tractography using convolutional neural network   DOI to the paper: link  ","categories": [],
+        "tags": ["MSMT-CSD","CNN","fODF","fiber tractography","deep learning"],
+        "url": "http://localhost:4000/Tinggong.github.io/CNN-fODF/",
+        "teaser": null
+      },{
+        "title": "MTE-NODDI",
+        "excerpt":"Multi-TE NODDI for disentangling non-T2-weighted signal fractions from compartment-specific T2 relaxation times   DOI to the paper: link  ","categories": [],
+        "tags": ["NODDI","T2 relaxation","Brain development","Brain maturation","Aging"],
+        "url": "http://localhost:4000/Tinggong.github.io/MTE-NODDI/",
+        "teaser": null
+      }]
