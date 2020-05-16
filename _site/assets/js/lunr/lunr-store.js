fb37674187b6965fe78107bf39919b228bee41e2
Ting Gong <tinggong@carolynas-mbp.home>
2020-05-16 16:55:17 +0100

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "MTE-NODDI",
-        "excerpt":"Multi-TE NODDI for disentangling non-T2-weighted signal fractions from compartment-specific T2 relaxation times DOI to the paper: link Conventional NODDI-derived compartment fractions are T2-weighted and TE-dependent MTE-NODDI disentangles non-T2-weighted signal fractions from T2 relaxation. Robust intra-neurite T2 estimation in WM even with CSF contamination. Non-T2-weighted fractions may improve the interpretability of...","categories": [],
+        "excerpt":"Multi-TE NODDI for disentangling non-T2-weighted signal fractions from compartment-specific T2 relaxation times DOI to the paper: link Conventional NODDI-derived compartment fractions are T2-weighted and TE-dependent MTE-NODDI disentangles non-T2-weighted signal fractions from T2 relaxation Robust estimates with reduced number of TE’s Non-T2-weighted fractions may improve the interpretability of neurodevelopmental studies For...","categories": [],
         "tags": ["NODDI","T2 relaxation","Brain development","Brain maturation","Aging"],
         "url": "http://localhost:4000/Tinggong.github.io/MTE-NODDI/",
         "teaser": "http://localhost:4000/Tinggong.github.io/images/Figure1.png"
