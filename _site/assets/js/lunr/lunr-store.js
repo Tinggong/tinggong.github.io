8d1a969299786dd0cad404abf174323a9a505802
Ting Gong <tinggong@carolynas-mbp.home>
2020-05-16 18:18:26 +0100

    image test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "CNN for fast fODF",
         "excerpt":"Fast learning of fiber orientation distribution function for MR tractography using convolutional neural network   DOI to the paper: link  ","categories": [],
         "tags": ["MSMT-CSD","CNN","fODF","fiber tractography","deep learning"],
-        "url": "http://localhost:4000/Tinggong.github.io/CNN-fODF/",
+        "url": "http://localhost:4000/CNN-fODF/",
         "teaser": null
       },{
         "title": "H-CNN for fast DKI",
         "excerpt":"Fast and robust diffusion kurtosis parametric mapping using a three-dimensional convolutional neural network   DOI to the paper: link  ","categories": [],
         "tags": ["DKI","CNN","hierarchical structure","deep learning"],
-        "url": "http://localhost:4000/Tinggong.github.io/H-CNN-DKI/",
+        "url": "http://localhost:4000/H-CNN-DKI/",
         "teaser": null
       },{
         "title": "MTE-NODDI",
         "excerpt":"Multi-TE NODDI for disentangling non-T2-weighted signal fractions from compartment-specific T2 relaxation times DOI to the paper: link Conventional NODDI-derived compartment fractions are T2-weighted and TE-dependent MTE-NODDI disentangles non-T2-weighted signal fractions from T2 relaxation Robust estimates with reduced number of TE’s Non-T2-weighted fractions may improve the interpretability of neurodevelopmental studies For...","categories": [],
         "tags": ["NODDI","T2 relaxation","Brain development","Brain maturation","Aging"],
-        "url": "http://localhost:4000/Tinggong.github.io/MTE-NODDI/",
-        "teaser": "http://localhost:4000/Tinggong.github.io/images/MTE-NODDI/Figure1.png"
+        "url": "http://localhost:4000/MTE-NODDI/",
+        "teaser": "http://localhost:4000/assets/images/MTE-NODDI/Figure1.png"
       }]
