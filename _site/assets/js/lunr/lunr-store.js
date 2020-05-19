864c70158f54c00bcf1a95e1a3fe5e870ab82457
Ting Gong <tinggong@carolynas-mbp.home>
2020-05-19 14:19:08 +0100

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "CNN for fast fODF",
-        "excerpt":"Fast learning of fiber orientation distribution function for MR tractography using convolutional neural network   DOI to the paper: link  ","categories": [],
+        "excerpt":"Fast learning of fiber orientation distribution function for MR tractography using convolutional neural network DOI to the paper: link CNN method gives more accurate and consistent estimates of fODFs when down-sampling DWI data Simulation data Angular errors and false rates on simulation data: voxels with 1, 2, and 3 fiber...","categories": [],
         "tags": ["MSMT-CSD","CNN","fODF","fiber tractography","deep learning"],
         "url": "http://localhost:4000/CNN-fODF/",
         "teaser": "http://localhost:4000/assets/images/CNN-fODF/Figure1.jpg"
@@ -15,5 +15,5 @@ var store = [{
         "excerpt":"Multi-TE NODDI for disentangling non-T2-weighted signal fractions from compartment-specific T2 relaxation times DOI to the paper: link Conventional NODDI-derived compartment fractions are T2-weighted and TE-dependent Demonstration of TE-dependence of NODDI-derived parameters in the set of chosen WM (A)(C) and GM (B) ROIs. As demonstrated above, fin increases with TE in...","categories": [],
         "tags": ["NODDI","T2 relaxation","Brain development","Brain maturation","Aging"],
         "url": "http://localhost:4000/MTE-NODDI/",
-        "teaser": "http://localhost:4000/assets/images/MTE-NODDI/Figure1.png"
+        "teaser": "http://localhost:4000/assets/images/MTE-NODDI/Figure4-1.png"
       }]
